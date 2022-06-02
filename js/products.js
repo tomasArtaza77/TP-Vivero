69b2288b54e1bcf170f8eace2e87ad5c365d20a3
tomas <tomas@TOMASA>
2022-06-02 18:16:27 -0300

    update productos y navegacion

--- js/products.js
+++ js/products.js
@@ -1,26 +1,94 @@
 const products = [
-    new Plant(1 , 'Peral', 'Pyrus communis', '-', 'Basico', '6 meses', 'Fila 1, Columna 1', 'ListaParaVenta'),
-    new Plant(2, 'Fresa', 'Fragaria', '-',	'Basico', '2 meses', 'Fila 1, Columna 2', 'EnLote'),
-    new Plant(3, 'Fresia', 'Freesia', 'Amarillo', 'Basico', '3 meses', 'Fila 2, Columna 1', 'EnExperimento'),
-    new Plant(4, 'Iris', 'Iris Germánica', 'Morado', 'Basico', 'Floracion', 'Fila 2, Columna 2', 'DeBaja'),
-    new Plant(5, 'Nardo sirio',	'Patrinia scabiosifolia', 'Dorado', 'Basico', '1 mes', 'Fila 3, Columna 1', 'ListaParaVenta'),
+    {
+        "id": 1, 
+        "img": '/img/plantas/1(Peral).jpg',
+        "nombre_comun": 'Peral', 
+        "nombre_cientifico": 'Pyrus communis', 
+        "color": '-', 
+        "tipo_suelo": 'Basico', 
+        "fase": '6 meses', 
+        "ubicacion": 'Fila 1, Columna 1', 
+        "estado": 'ListaParaVenta'
+    },
+    {
+        "id": 2, 
+        "img": '/img/plantas/2(Fragaria).jpg',
+        "nombre_comun": 'Fresa', 
+        "nombre_cientifico": 'Fragaria', 
+        "color": '-', 
+        "tipo_suelo": 'Basico', 
+        "fase": '2 meses', 
+        "ubicacion": 'Fila 1, Columna 2', 
+        "estado": 'EnLote'
+    },
+    {
+        "id": 3,
+        "img": '/img/plantas/3(Freesia).jpg',
+        "nombre_comun": 'Fresia', 
+        "nombre_cientifico": 'Freesia', 
+        "color": 'Amarillo', 
+        "tipo_suelo": 'Basico', 
+        "fase": '2 meses', 
+        "ubicacion": 'Fila 2, Columna 1', 
+        "estado": 'EnExperimento'
+    },
+    {
+        "id": 4, 
+        "img": '/img/plantas/4(Iris Germánica).jpg',
+        "nombre_comun": 'Iris', 
+        "nombre_cientifico": 'Iris Germánica', 
+        "color": 'Morado', 
+        "tipo_suelo": 'Basico', 
+        "fase": 'Floracion', 
+        "ubicacion": 'Fila 2, Columna 2', 
+        "estado": 'DeBaja'
+    },
+    {
+        "id": 5, 
+        "img": '/img/plantas/5(Patrinia scabiosifolia).jpg',
+        "nombre_comun": 'Nardo sirio', 
+        "nombre_cientifico": 'Patrinia scabiosifolia', 
+        "color": 'Dorado', 
+        "tipo_suelo": 'Basico', 
+        "fase": '1 mes', 
+        "ubicacion": 'Fila 3, Columna 1', 
+        "estado": 'ListaParaVenta'
+    },
+
 ];
+
 
 function showTable(params) {
     buildTable(products);
 }
+
 
 function buildTable(data){
     console.log(data);
     let table = '';
     for (let i = 0; i < data.length; i++) {
         table += 
-        `<li>
-            <td>${data[i].id}</td>
-            <td>${data[i].nombre_comun}</td>
-            <td>${data[i].nombre_cientifico}</td>
+        `
+        <div class="container mt-5">
+            <div class = "row">
+                <div class = "column"> 
+                    <div class = "card"> 
+                        <p>N°: ${data[i].id}</p>
+                        <img [src]="data[i].img" alt="Products">
+                        <p>Name: ${data[i].nombre_comun}</p>
+                        <p>Nombre científico: ${data[i].nombre_cientifico}</p>
+                        <p>Color: ${data[i].color}</p>
+                        <p>Tipo de suelo: ${data[i].tipo_suelo}</p>
+                        <p>Fase: ${data[i].fase}</p>
+                        <p>Ubicación: ${data[i].ubicacion}</p>
+                        <p>Estado: ${data[i].estado}</p>
+                    </div>
+                </div>
+            </div>
+	    </div>
+        `
+        ;
 
-        </li>`;
         console.log(table);
         document.getElementById('products-page').innerHTML = table;
     }
